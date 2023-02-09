e9cfdd6247ea0306ca528f93259cd41d26e90560
Shaokun-X <shaokun.xie@outlook.fr>
2023-02-09 13:36:04 +0100

    direct key-to-pin

--- pedal_midi.ino
+++ pedal_midi.ino
@@ -3,10 +3,13 @@
 */
 
 #include <MIDIUSB.h>
-#include <pitchToNote.h>
 
-#define NUM_COLUMNS 4
-#define NUM_ROWS 8
+#define PITCH_C1 24
+
+#define NUM_PEDALS 30
+
+// in micro seconds
+#define BOUNCE_WINDOW 500
 
 // Use fixed intensity, the range is 0-127
 #define INTENSITY 127
@@ -14,53 +17,60 @@
 // Data is not sent until buffer is full, size unit in bytes, should be greater than or equal to 4 (size of midiEventPacket_t)
 // #define BUFFER_SIZE 64
 
-
 // Define your pin numbers here
-const uint8_t pedal1 = 2;
-const uint8_t pedal2 = 3;
-const uint8_t pedal3 = 4;
-const uint8_t pedal4 = 5;
-const uint8_t pedal5 = 6;
-const uint8_t pedal6 = 7;
-const uint8_t pedal7 = 8;
-const uint8_t pedal8 = 9;
+#define PEDAL1 22
+#define PEDAL2 23
+#define PEDAL3 24
+#define PEDAL4 25
+#define PEDAL5 26
+#define PEDAL6 27
+#define PEDAL7 28
+#define PEDAL8 29
+#define PEDAL9 30
+#define PEDAL10 31
+#define PEDAL11 32
+#define PEDAL12 33
+#define PEDAL13 34
+#define PEDAL14 35
+#define PEDAL15 36
+#define PEDAL16 37
+#define PEDAL17 38
+#define PEDAL18 39
+#define PEDAL19 40
+#define PEDAL20 41
+#define PEDAL21 42
+#define PEDAL22 43
+#define PEDAL23 44
+#define PEDAL24 45
+#define PEDAL25 46
+#define PEDAL26 47
+#define PEDAL27 48
+#define PEDAL28 49
+#define PEDAL29 50
+#define PEDAL30 51
 
-const uint8_t group1 = 10;
-const uint8_t group2 = 11;
-const uint8_t group3 = 12;
-const uint8_t group4 = 13;
+const uint8_t pedals[NUM_PEDALS] = {
+    PEDAL1, PEDAL2, PEDAL3, PEDAL4, PEDAL5, PEDAL6, PEDAL7, PEDAL8,
+    PEDAL9, PEDAL10, PEDAL11, PEDAL12, PEDAL13, PEDAL14, PEDAL15, PEDAL16,
+    PEDAL17, PEDAL18, PEDAL19, PEDAL20, PEDAL21, PEDAL22, PEDAL23, PEDAL24,
+    PEDAL25, PEDAL26, PEDAL27, PEDAL28, PEDAL29, PEDAL30};
 
-const uint8_t pedals[NUM_ROWS] = {pedal1, pedal2, pedal3, pedal4, pedal5, pedal6, pedal7, pedal8};
-const uint8_t groups[NUM_COLUMNS] = {group1, group2, group3, group4};
+// one bit per pedal
+unsigned long int pressedPedals = 0;
+unsigned long int previousPedals = 0;
 
-uint8_t pressedPedals[NUM_COLUMNS] = {0, 0, 0, 0};
-
-uint8_t previousPedals[NUM_COLUMNS] = {0, 0, 0, 0};
+unsigned long lastTriggerTime[NUM_PEDALS];
 
 // unsigned int bufferCount = 0;
-
 
 /*
  * Calculate pitch from column and row numbers
  * col: from 0 to NUM_COLUMNS-1
  * row: from 0 to NUM_ROWS-1
  */
-uint8_t getPitch(uint8_t col, uint8_t row) {
-  return pitchC1 + col * 8 + row;
-}
-
-/*
- * Set target column pin to HIGH the other to low
- * col: from 0 to NUM_COLUMNS-1
- */
-void enableColumn(uint8_t col) {
-  for (int i = 0; i < NUM_COLUMNS; i++) {
-    if (col == i) {
-      digitalWrite(groups[i], LOW);
-    } else {
-      digitalWrite(groups[i], HIGH);
-    }
-  }
+uint8_t getPitch(int n)
+{
+  return PITCH_C1 + n;
 }
 
 // First parameter is the event type (0x09 = note on, 0x08 = note off).
@@ -68,13 +78,15 @@ void enableColumn(uint8_t col) {
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
 // Third parameter is the note number (48 = middle C).
 // Fourth parameter is the velocity (64 = normal, 127 = fastest).
-void noteOn(byte channel, byte pitch, byte velocity) {
+void noteOn(byte channel, byte pitch, byte velocity)
+{
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
   // bufferCount += sizeof(midiEventPacket_t);
 }
 
-void noteOff(byte channel, byte pitch, byte velocity) {
+void noteOff(byte channel, byte pitch, byte velocity)
+{
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
   // bufferCount += sizeof(midiEventPacket_t);
@@ -85,50 +97,63 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 //   bufferCount = 0;
 // }
 
-
 ////////////////////////////////////////////
 //////////   reserved functions   //////////
 ////////////////////////////////////////////
-void setup() {
-  for (int i = 0; i < NUM_ROWS; i++)
+void setup()
+{
+  for (int i = 0; i < NUM_PEDALS; i++)
+  {
     pinMode(pedals[i], INPUT_PULLUP);
-
-  for (int i = 0; i < NUM_COLUMNS; i++)
-    pinMode(groups[i], OUTPUT);
+    lastTriggerTime[i] = 0;
+  }
 }
 
-void loop() {
+void loop()
+{
 
-  // Read pedals
-  for (int i = 0; i < NUM_COLUMNS; i++) {
-    enableColumn(i);
-    for (int j = 0; j < NUM_ROWS; j++) {
-      if (digitalRead(pedals[j]) == LOW) {
-        bitWrite(pressedPedals[i], j, 1);
-      } else {
-        bitWrite(pressedPedals[i], j, 0);
+  // Read debounced pedals
+  for (int i = 0; i < NUM_PEDALS; i++)
+  {
+    if (lastTriggerTime[i] == 0) {
+      // first check
+      if (digitalRead(pedals[i]) == LOW) {
+        lastTriggerTime[i] = micros();
+      } 
+      else 
+      {
+        // no need to check release
+        bitWrite(pressedPedals, i, 0);
+      }
+    }
+    else
+    {
+      // when elapsed time reaches the threshold, do a second check
+      if (micros() - lastTriggerTime[i] > BOUNCE_WINDOW)
+      {
+        lastTriggerTime[i] = 0;
+        if (digitalRead(pedals[i]) == LOW) bitWrite(pressedPedals, i, 1);
       }
     }
   }
 
-  // Queue events
-  for (int i = 0; i < NUM_COLUMNS; i++) {
-    for (int j = 0; j < NUM_ROWS; j++) {
-
-      if (bitRead(pressedPedals[i], j) != bitRead(previousPedals[i], j)) {
-        if (bitRead(pressedPedals[i], j)) {
+  for (int i = 0; i < NUM_PEDALS; i++)
+  {
+    if (bitRead(pressedPedals, i) != bitRead(previousPedals, i))
+    {
+      if (bitRead(pressedPedals, i)) {
           // press
-          bitWrite(previousPedals[i], j , 1);
-          noteOn(0, getPitch(i, j), INTENSITY);
+          bitWrite(previousPedals, i, 1);
+          noteOn(0, getPitch(i), INTENSITY);
           MidiUSB.flush();
-        } else {
-          // release
-          bitWrite(previousPedals[i], j , 0);
-          noteOff(0, getPitch(i, j), 0);
-          MidiUSB.flush();
-        }
       }
-
+      else
+      {
+          // release
+          bitWrite(previousPedals, i, 0);
+          noteOff(0, getPitch(i), 0);
+          MidiUSB.flush();
+      }
     }
   }
 
